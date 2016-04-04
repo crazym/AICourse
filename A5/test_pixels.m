b4cbc8246f4971e1c9c16bfbfe7754e3ecc892a2
Minty Zhang <minty.mz@gmail.com>
2016-04-04 11:27:43 -0400

    tests on pixels

--- A5/test_pixels.m
+++ A5/test_pixels.m
@@ -28,8 +28,8 @@ function [idx1]=test_pixels(pix1,pix2,testID)
 
 	switch(testID)
 		case 0
-			idx1=find(pix1==5);			% Dummy - gives the indexes of all pixels
-										% in input vector pix1 whose value is 5.
+			% indices of pixels in input vector pix1 whose value is greater than 200.
+			idx1=find(pix1>200);			
 		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		%	TO DO:
 		%		  Add your own tests. You can add as many as you like, but
@@ -43,6 +43,18 @@ function [idx1]=test_pixels(pix1,pix2,testID)
 		%         Said a different way. There should be *no* for loops in this
 		%		 script.
 		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		case 1 
+			%both  black
+			idx1 = find(pix1 > 200 & pix2 > 200);
+		case 2
+			%both white
+			idx1 = find(pix1 < 10 & pix1 < 10);
+		case 2
+			% small the color difference
+			idx1 = find(abs(pix1 - pix2) < 10);
+		case 3
+			%large color difference
+			idx1 = find(abs(pix1 - pix2) > 200);
 		otherwise
 			fprintf(2,'Invalid testID!\n'); idx1=[]; return;
 	end;
